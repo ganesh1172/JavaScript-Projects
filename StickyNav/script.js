9c7bfa1c23d19c39b8bbdc6515820700a13748df
ganesh1172 <ganesh172595@gmail.com>
2024-02-01 16:09:33 +0530

    create new project 05-Sticky Nav

---
+++ StickyNav/script.js
@@ -0,0 +1,23 @@
+const navElement = document.querySelector("nav");
+const navBarLinks = document.querySelectorAll("a");
+
+const navPosition = navElement.getBoundingClientRect().top;
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  navElement.style.top = scrollPosition + "px";
+
+  navBarLinks.forEach((link) => {
+    const sectionElement = document.querySelector(link.hash);
+    const offset = 50;
+    if (
+      scrollPosition + offset > sectionElement.offsetTop &&
+      scrollPosition + offset <
+        sectionElement.offsetTop + sectionElement.offsetHeight
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
